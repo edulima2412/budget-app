22ebcea5148fec33d422e022a0e15484df2c9614
Eduardo Lima <edulima2412@gmail.com>
2021-03-27 13:40:05 -0300

    finished delete items

--- budget-app/src/app/budget-item-list/budget-item-list.component.ts
+++ budget-app/src/app/budget-item-list/budget-item-list.component.ts
@@ -1,5 +1,5 @@
 import { BudgetItem } from './../../shared/models/budget-item.model';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-budget-item-list',
@@ -9,10 +9,15 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BudgetItemListComponent implements OnInit {
 
   @Input() budgetItems: BudgetItem[];
+  @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(item: BudgetItem) {
+    this.delete.emit(item);
   }
 
 }
